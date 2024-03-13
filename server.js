681ebcff73a6820ed8be11590f70dd88df3576dc
Phirapong <64160031@go.buu.ac.th>
2024-03-13 20:54:41 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const database = require("./database/connection");
+database.openConnection();
+
+const UserController = require("./feature/user/controller");
+
+app.use(express.json()); // for parsing application/json
+app.use('/user', UserController);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
